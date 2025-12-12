87b93529e6c8f3b430163c1c6381b8714bd0ceab
Tacila Souza <tacilasouza.contato@gmail.com>
2025-12-12 14:08:50 -0300

    Menu inicial criado

--- Menu.ts
+++ Menu.ts
@@ -6,21 +6,18 @@ export function main() {
 
     while (true) {
     
-       console.log("*****************************************************");
+        console.log("*****************************************************");
         console.log("                                                     ");
-        console.log("                BANCO DO BRAZIL COM Z                ");
+        console.log("                GESTAO DE ESTOQUE                    ");
         console.log("                                                     ");
         console.log("*****************************************************");
         console.log("                                                     ");
-        console.log("            1 - Criar Conta                          ");
-        console.log("            2 - Listar todas as Contas               ");
-        console.log("            3 - Buscar Conta por Numero              ");
-        console.log("            4 - Atualizar Dados da Conta             ");
-        console.log("            5 - Apagar Conta                         ");
-        console.log("            6 - Sacar                                ");
-        console.log("            7 - Depositar                            ");
-        console.log("            8 - Transferir valores entre Contas      ");
-        console.log("            9 - Sair                                 ");
+        console.log("            1 - Adicionar produto                    ");
+        console.log("            2 - Listar todos os produtos             ");
+        console.log("            3 - Buscar produto por ID                ");
+        console.log("            4 - Atualizar informacoes do produto     ");
+        console.log("            5 - Excluir produto                      ");
+        console.log("            6 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
         console.log("                                                     ");
@@ -30,37 +27,30 @@ export function main() {
 
         switch (opcao) {
             case 1:
-                console.log("\n\nCriar Conta\n\n");
+                console.log("\n\nAdicionar produto\n\n");
 
                 break;
             case 2:
-                console.log("\n\nListar todas as Contas\n\n");
+                console.log("\n\nListar todos os produtos\n\n");
 
                 break;
             case 3:
-                console.log("\n\nConsultar dados da Conta - por número\n\n");
+                console.log("\n\nBuscar produto por ID\n\n");
 
                 break;
             case 4:
-                console.log("\n\nAtualizar dados da Conta\n\n");
+                console.log("\n\nAtualizar informacoes do produto\n\n");
 
                 break;
             case 5:
-                console.log("\n\nApagar uma Conta\n\n");
+                console.log("\n\nExcluir produto\n\n");
 
                 break;
             case 6:
-                console.log("\n\nSaque\n\n");
+                console.log("\n\nSair\n\n");
 
                 break;
-            case 7:
-                console.log("\n\nDepósito\n\n");
-
-                break;
-            case 8:
-                console.log("\n\nTransferência entre Contas\n\n");
-
-                break;
+                
             default:
                 console.log("\nOpção Inválida!\n");
 
